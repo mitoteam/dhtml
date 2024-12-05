4f579edbd9cd670324a8fd3db3fa75bd49ddee7d
Mikhail Oleynik <mishutka@mito-team.com>
2024-12-05 17:11:40 +0300

    div test

--- tag_test.go
+++ tag_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func TestDiv(t *testing.T) {
-	div := Div().Class("cls")
-	html := "<div class=\"cls\"></div>"
+	div := Div().Id("ay_di").Class("cls").Title("title").Attribute("a", "b c").Attribute("d", "").Text("text")
+	html := "<div id=\"ay_di\" class=\"cls\" a=\"b c\" d title=\"title\">text</div>"
 
 	require.Equal(t, html, div.String())
 }
