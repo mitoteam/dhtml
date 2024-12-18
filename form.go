e5b8f4f01206881d8389f00a7c74921aecaa9467
Mikhail Oleynik <mishutka@mito-team.com>
2024-12-18 18:47:13 +0300

    forms handling moved to dhtmlform

--- form.go
+++ form.go
@@ -1,8 +1,7 @@
 package dhtml
 
 type FormElement struct {
-	formData *FormData
-	tag      *Tag
+	tag *Tag
 }
 
 // force interfaces implementation
@@ -11,7 +10,7 @@ var _ ElementI = (*FormElement)(nil)
 // Html form just to render it
 func NewForm() *FormElement {
 	return &FormElement{
-		tag: NewTag("form").Attribute("method", "post"),
+		tag: NewTag("form"),
 	}
 }
 
@@ -20,37 +19,17 @@ func (f *FormElement) Class(v ...any) *FormElement {
 	return f
 }
 
+func (f *FormElement) Method(method string) *FormElement {
+	f.tag.Attribute("method", method)
+	return f
+}
+
 func (f *FormElement) Append(v ...any) *FormElement {
 	for _, v := range v {
-		if f.formData != nil { //managed form
-			if e, ok := v.(FormItemI); ok {
-				fd := f.formData //shorthand pointer
-
-				if value, ok := fd.values.GetOk(e.GetName()); ok {
-					e.SetValue(value)
-				} else {
-					fd.values.Set(e.GetName(), "") //add empty string to data
-				}
-
-				fd.labels.Set(e.GetName(), e.GetLabel())
-
-				//TODO: should not be here at all...
-				if e, ok := v.(*FormInputElement); ok {
-					if e.tag.GetAttribute("type") == "checkbox" {
-						fd.checkboxList = append(fd.checkboxList, e.GetName())
-					}
-				}
-			}
-		}
-
 		f.tag.Append(v)
 	}
 
 	return f
-}
-
-func (f *FormElement) GetFormData() *FormData {
-	return f.formData
 }
 
 func (f *FormElement) GetTags() TagList {
--- settings.go
+++ settings.go
@@ -3,12 +3,6 @@ package dhtml
 // dhtml settings to be overridden from outside packages
 
 type settingsType struct {
-	//TODO: SubmitElementRendererF func() FormSubmitElement
-	DefaultSubmitButtonClasses Classes
-
-	// function to render errors block if there are any after form validation
-	FormErrorsRendererF func(fd *FormData) (out HtmlPiece)
-
 	EmptyLabelRendererF func(label string, span *Tag)
 }
 
@@ -21,29 +15,6 @@ func Settings() *settingsType {
 // default implementations
 func init() {
 	settings = &settingsType{
-		FormErrorsRendererF: func(fd *FormData) (out HtmlPiece) {
-			container := Div().Class("form-errors")
-
-			for name, itemErrors := range fd.errorList {
-				for _, itemError := range itemErrors {
-					errorOut := Div().Class("item-error")
-
-					if name != "" {
-						errorOut.Attribute("data-form-item-name", name).
-							Append(Span().Append(fd.GetLabel(name))).
-							Text(":")
-					}
-
-					errorOut.Append(itemError)
-
-					container.Append(errorOut)
-				}
-			}
-
-			out.Append(container)
-			return out
-		},
-
 		EmptyLabelRendererF: func(label string, span *Tag) {
 			if label == "" {
 				label = "empty"
